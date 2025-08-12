153da5bc86b3bc6e04e8989d1844a5ae43ecd7cf
rma-toni <rmaflores.sw@gmail.com>
2025-08-12 17:06:07 -0300

    init

---
+++ src/Basics.java
@@ -0,0 +1,19 @@
+public class Basics {
+
+    //Variable names are case-sensitive on Java and most languages
+    //letters, digits, dollar signs, or underscore
+    //static
+    static int defaultSpeed = 1;
+
+    public static void main(String[] args) {
+        //non static
+        int speed = 0;
+        System.out.println("Hello World!");
+    }
+
+    //method with a parameter
+    void method_1(int speed){
+        //local var
+        int strength = 10;
+    }
+}
